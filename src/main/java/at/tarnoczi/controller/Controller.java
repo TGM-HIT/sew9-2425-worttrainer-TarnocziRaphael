1e609909e6ed9725ff2bf69b0b177b16e8710eb6
Raphael Tarnoczi <raphi.tarnoczi@gmail.com>
2024-09-16 15:15:51 +0200

    created files for view

---
+++ src/main/java/at/tarnoczi/controller/Controller.java
@@ -0,0 +1,10 @@
+package at.tarnoczi.controller;
+
+/**
+ * Beschreibung des Programms
+ *
+ * @author Raphael Tarnoczi
+ * @version 2024-09-16
+ */
+public class Controller {
+}
---
+++ src/main/java/at/tarnoczi/view/Frame.java
@@ -0,0 +1,10 @@
+package at.tarnoczi.view;
+
+/**
+ * Beschreibung des Programms
+ *
+ * @author Raphael Tarnoczi
+ * @version 2024-09-16
+ */
+public class Frame {
+}
